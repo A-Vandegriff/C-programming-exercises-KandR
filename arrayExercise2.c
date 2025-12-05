442dbbaff6a332f6f898f383f66694adcf8ff869
A-Vandegriff <austinvandegriff@yahoo.com>
2025-12-05 16:10:27 -0500

    found all the visible characters when outputting %c

--- arrayExercise2.c
+++ arrayExercise2.c
@@ -1,3 +1,16 @@
+#include <stdio.h>
+
+#define CHARS 256
+
 int main(void){
 
+int i;
+int chars[CHARS];
+
+	for(i = 0; i < CHARS; i++){
+		chars[i] = 0;
+	}
+	for(i = 33; i < 127 ; i++){
+		printf("%c: \n", i);
+	}
 }
