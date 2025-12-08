b0123c25dd56f2876ecb12e04d0a4237e94078f6
A-Vandegriff <austinvandegriff@yahoo.com>
2025-12-07 22:11:46 -0500

    finished arrayExercise2, will optimize

--- arrayExercise2.c
+++ arrayExercise2.c
@@ -4,13 +4,24 @@
 
 int main(void){
 
-int i;
+int c,i,j;
 int chars[CHARS];
 
 	for(i = 0; i < CHARS; i++){
 		chars[i] = 0;
 	}
-	for(i = 33; i < 127 ; i++){
-		printf("%c: \n", i);
+
+	while((c = getchar()) != EOF){
+		if(c >= 33 && c < 127){
+			chars[c]++;
+        	}
+	}
+	for(i = 33;i<127;i++){
+		printf("%c:", i);
+		for(j = 0; j<chars[i]; j++){
+			printf("-");
+		}
+		printf("\n");
 	}
 }
+
