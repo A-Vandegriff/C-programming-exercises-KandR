2b984293dcf7a8eabab7ed2d5a90ad6563fa566e
A-Vandegriff <austinvandegriff@yahoo.com>
2025-12-16 02:07:53 -0500

    wrote function outlines for char array section 1.9

---
+++ charArrays.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+int getline(char line[], int maxline);
+void copy(char to[], char from[]);
+
+int main(void){
+
+}
+
+int getline(char s[], int lim){
+
+}
+
+void copy(char to[], char from[]){
+
+
+}
