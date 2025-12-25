a372b52485bc39de0e22e97e946f301153a7bc29
A-Vandegriff <austinvandegriff@yahoo.com>
2025-12-25 16:34:50 -0500

    set up functions for exercise 1.17

--- exercise-1.17.c
+++ exercise-1.17.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
+#define MAXLENGTH 1000
+
+int get_line(int arr[], int lim);
+void copy(int longest[], int line[]);
+
 int main(void){
 
+
 return 0;
+}
+
+int get_line(int arr[], int lim){
+
+
+}
+
+void copy(int to[], int from[]){
+
+
 }
