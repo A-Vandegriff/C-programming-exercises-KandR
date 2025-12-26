cb5e580c60fc510d6f38b255134935df4a2ad16a
A-Vandegriff <austinvandegriff@yahoo.com>
2025-12-26 14:04:22 -0500

    finished exercise 1.17

--- exercise-1.17.c
+++ exercise-1.17.c
@@ -2,21 +2,29 @@
 
 #define MAXLENGTH 1000
 
-int get_line(int arr[], int lim);
-void copy(int longest[], int line[]);
+int get_line(char arr[], int lim);
 
 int main(void){
-
-
+int len;
+char line[MAXLENGTH];
+	while((len = get_line(line, MAXLENGTH)) > 0){
+		if(len > 80){
+				printf("%s", line);
+		}
+	}
 return 0;
 }
 
-int get_line(int arr[], int lim){
-
-
+int get_line(char s[], int lim){
+int c, i;
+	for(i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n';i++){
+	  s[i] = c;
+	}
+	if(c == '\n'){
+		s[i] = c;
+		i++;
+	}
+	s[i] = '\0';
+	return i;
 }
 
-void copy(int to[], int from[]){
-
-
-}
