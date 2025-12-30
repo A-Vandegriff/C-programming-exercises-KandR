e27d1567ab36394fe8a5e3f5bfadc24d7449ca39
A-Vandegriff <austinvandegriff@yahoo.com>
2025-12-30 10:52:13 -0500

    added test file to see hidden char, working on main in 1.18

--- exercise-1.18.c
+++ exercise-1.18.c
@@ -2,24 +2,36 @@
 
 #define MAXLINE 1000
 
-int get_line(int s[], int lim);
+int get_line(char arr[], int lim);
 
 int main(void){
+int len;
+char arr[MAXLINE];
 
+while((len = get_line(arr, MAXLINE)) > 0){
 
+while(len > 0 && (arr[len-1] == '\t' || arr[len-1] == ' ')){
+arr[--len] = '\0';
+}
+if(len > 0 && arr[0] != '\n'){
+printf("%s", arr);
+}
+}
 return 0;
 }
 
-int get_line(int s[], int lim){
+
+
+int get_line(char s[], int lim){
 int c,i;
 
-for(i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'){
+for(i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; i++){
 	s[i] = c;
 }
 if(c == '\n'){
 s[i] = '\n';
 i++;
 }
-s[i] = '\0'
+s[i] = '\0';
 return i;
 }
