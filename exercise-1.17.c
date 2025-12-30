7a9e54fc36fa957ba6d1c27d5ec382eecb579c55
A-Vandegriff <austinvandegriff@yahoo.com>
2025-12-29 21:44:29 -0500

    formatted 1.17 and wrote get_line for 1.18

--- exercise-1.17.c
+++ exercise-1.17.c
@@ -4,27 +4,26 @@
 
 int get_line(char arr[], int lim);
 
-int main(void){
-int len;
-char line[MAXLENGTH];
-	while((len = get_line(line, MAXLENGTH)) > 0){
-		if(len > 80){
-				printf("%s", line);
-		}
-	}
-return 0;
+int main(void) {
+  int len;
+  char line[MAXLENGTH];
+  while ((len = get_line(line, MAXLENGTH)) > 0) {
+    if (len > 80) {
+      printf("%s", line);
+    }
+  }
+  return 0;
 }
 
-int get_line(char s[], int lim){
-int c, i;
-	for(i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n';i++){
-	  s[i] = c;
-	}
-	if(c == '\n'){
-		s[i] = c;
-		i++;
-	}
-	s[i] = '\0';
-	return i;
+int get_line(char s[], int lim) {
+  int c, i;
+  for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++) {
+    s[i] = c;
+  }
+  if (c == '\n') {
+    s[i] = c;
+    i++;
+  }
+  s[i] = '\0';
+  return i;
 }
-
--- exercise-1.18.c
+++ exercise-1.18.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
+#define MAXLINE 1000
+
+int get_line(int s[], int lim);
+
 int main(void){
 
+
 return 0;
+}
+
+int get_line(int s[], int lim){
+int c,i;
+
+for(i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'){
+	s[i] = c;
+}
+if(c == '\n'){
+s[i] = '\n';
+i++;
+}
+s[i] = '\0'
+return i;
 }
