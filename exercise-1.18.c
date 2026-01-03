15ce668adbb4b50f5fd065c9a765df2c302bb311
A-Vandegriff <austinvandegriff@yahoo.com>
2026-01-03 02:49:59 -0500

    formatted codebase

--- exercise-1.18.c
+++ exercise-1.18.c
@@ -4,36 +4,34 @@
 
 int get_line(char arr[], int lim);
 
-int main(void){
-int len;
-char arr[MAXLINE];
+int main(void) {
+  int len;
+  char arr[MAXLINE];
 
-while((len = get_line(arr, MAXLINE)) > 0){
-if(len > 0 && arr[len-1] == '\n'){
-	arr[--len] = '\0';
-}
-while(len > 0 && (arr[len-1] == '\t' || arr[len-1] == ' ')){
-arr[--len] = '\0';
-}
-if(len > 0 && arr[0] != '\n'){
-printf("%s\n", arr);
-}
-}
-return 0;
+  while ((len = get_line(arr, MAXLINE)) > 0) {
+    if (len > 0 && arr[len - 1] == '\n') {
+      arr[--len] = '\0';
+    }
+    while (len > 0 && (arr[len - 1] == '\t' || arr[len - 1] == ' ')) {
+      arr[--len] = '\0';
+    }
+    if (len > 0 && arr[0] != '\n') {
+      printf("%s\n", arr);
+    }
+  }
+  return 0;
 }
 
+int get_line(char s[], int lim) {
+  int c, i;
 
-
-int get_line(char s[], int lim){
-int c,i;
-
-for(i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; i++){
-	s[i] = c;
-}
-if(c == '\n'){
-s[i] = '\n';
-i++;
-}
-s[i] = '\0';
-return i;
+  for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++) {
+    s[i] = c;
+  }
+  if (c == '\n') {
+    s[i] = '\n';
+    i++;
+  }
+  s[i] = '\0';
+  return i;
 }
