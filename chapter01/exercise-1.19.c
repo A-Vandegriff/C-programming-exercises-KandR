fc64abedd986c982d4273cb2357edf7aa20894f9
A-Vandegriff <austinvandegriff@yahoo.com>
2026-01-05 00:37:17 -0500

    formatted codebase AND made swapping more efficient (two pointer)

--- chapter01/exercise-1.19.c
+++ chapter01/exercise-1.19.c
@@ -5,44 +5,41 @@
 void reverse(char arr[], int len);
 int get_line(char arr[], int lim);
 
-int main(void){
-int len;
-char arr[MAXLENGTH];
+int main(void) {
+  int len;
+  char arr[MAXLENGTH];
 
-while((len = get_line(arr, MAXLENGTH)) > 0){
-	if(len > 0 && arr[len-1] == '\n'){
-		reverse(arr,len-1);
-	}else{
-		reverse(arr, len);
-	}
-	printf("%s", arr);
+  while ((len = get_line(arr, MAXLENGTH)) > 0) {
+    if (len > 0 && arr[len - 1] == '\n') {
+      reverse(arr, len - 1);
+    } else {
+      reverse(arr, len);
+    }
+    printf("%s", arr);
+  }
+
+  return 0;
 }
 
-return 0;
+void reverse(char s[], int len) {
+  int i, temp;
+
+  for (i = 0; i < len / 2; i++) {
+    temp = s[i];
+    s[i] = s[len - 1 - i];
+    s[len - 1 - i] = temp;
+  }
 }
+int get_line(char s[], int lim) {
+  int c, i;
 
-void reverse(char s[], int len){
-int i, temp;
-
-for(i = 0; i < len; i++){
-temp = s[i];
-s[i] = s[len-1];
-s[len-1] = temp;
-len--;
-
-}
-
-}
-int get_line(char s[], int lim){
-int c, i;
-
-for(i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; i++){
-	s[i] = c;
-}
-	if(c == '\n'){
-		s[i] = c;
-		i++;
-	}
-	s[i] = '\0';
-	return i;
+  for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++) {
+    s[i] = c;
+  }
+  if (c == '\n') {
+    s[i] = c;
+    i++;
+  }
+  s[i] = '\0';
+  return i;
 }
