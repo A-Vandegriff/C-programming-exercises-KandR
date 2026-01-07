ae453ef910a0e5db1bc4fcb39c8cc1aa5c4b37a2
A-Vandegriff <austinvandegriff@yahoo.com>
2026-01-07 12:20:46 -0500

    finished 1.20, formatted codebase

--- chapter01/exercise-1.20.c
+++ chapter01/exercise-1.20.c
@@ -3,36 +3,32 @@
 #define TABSTOP 8
 #define MAXLENGTH 1000
 
-
 /* realized I need to make a state machine to process
 individual charaters from a stream instead of processing data from a buffer*/
 
+int main(void) {
+  int c;
+  int col;
+  int blanks;
 
-int main(void){
-int c;
-int col;
-int blanks;
+  col = 0;
+  blanks = 0;
 
-
-col = 0;
-blanks = 0;
-
-while((c = getchar()) != EOF){
-	if(c == '\t'){
-		blanks = TABSTOP - (col % TABSTOP);
-		col += blanks;
-		while(blanks > 0){
-			printf(" ");
-			blanks--;
-		}
-	}else if(c == '\n'){
-		col = 0;
-		printf("\n");
-	}else{
-		printf("%c", c);
-		col++;
-	}
+  while ((c = getchar()) != EOF) {
+    if (c == '\t') {
+      blanks = TABSTOP - (col % TABSTOP);
+      col += blanks;
+      while (blanks > 0) {
+        printf(" ");
+        blanks--;
+      }
+    } else if (c == '\n') {
+      col = 0;
+      printf("\n");
+    } else {
+      printf("%c", c);
+      col++;
+    }
+  }
+  return 0;
 }
-return 0;
-}
-
