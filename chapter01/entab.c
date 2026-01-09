aac83a9901fc40bb22143baf09aed017446a7be8
A-Vandegriff <austinvandegriff@yahoo.com>
2026-01-09 15:10:43 -0500

    set up entab as a state machine, logic is next

---
+++ chapter01/entab.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define TABSTOP 8
+
+
+int main(void){
+int c;
+int col;
+int blanks;
+
+while((c = getchar()) != EOF){
+	if(c == '\n'){
+		col = 0;
+	}else if(c == ' '){
+		
+	}else if(c == '\t'){
+		
+	}else{
+		col++;
+		printf("%c", c);
+	}
+
+}
+
+return 0;
+}
