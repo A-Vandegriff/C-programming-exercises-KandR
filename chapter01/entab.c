ae76d6edc3b2a1df5f89cadc57b04d123edcd375
A-Vandegriff <austinvandegriff@yahoo.com>
2026-01-12 00:41:01 -0500

    need to fix program stream, also need to change tabs to in terms of TABSTOPS

--- chapter01/entab.c
+++ chapter01/entab.c
@@ -8,6 +8,10 @@ int main(void){
 int c;
 int col;
 int blanks;
+int tabs;
+tabs = 0;
+blanks = 0;
+col = 0;
 
 while((c = getchar()) != EOF){
 	if(c == '\n'){
@@ -15,8 +19,22 @@ while((c = getchar()) != EOF){
 	}else if(c == ' '){
 		while(c == ' '){
 			blanks++;
-			col++;
+			c = getchar();
 		}
+		if(blanks >= 8){
+			tabs = blanks/8;
+			col += tabs*8;
+			while(tabs > 0){
+				printf("\t");
+				tabs--;
+			}
+		}
+		while((blanks % TABSTOP) > 0){
+			printf(" ");
+			col++;
+			blanks--;
+		}
+		
 	}else if(c == '\t'){
 		
 	}else{
