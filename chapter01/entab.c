16a4a8c170baa7177aa52f7e46a89cc0e5ef871d
A-Vandegriff <austinvandegriff@yahoo.com>
2026-01-12 23:39:33 -0500

    finished entab with help, will redo by myself in future

--- chapter01/entab.c
+++ chapter01/entab.c
@@ -1,22 +1,49 @@
 /* write a program entab that replaces strings of blanks by the
 minimum number of tabs and blanks to acheive the same spacing*/
-
 #include <stdio.h>
-
 
 #define TABSTOP 8
 
+int main(void)
+{
+    int c;
+    int col = 0;      /* current column */
+    int blanks = 0;   /* pending blanks */
 
-int main(void){
-int c;
-int col;
-int blanks;
-int tabs;
-col = 0;
-while((c = getchar()) != EOF){
+    while ((c = getchar()) != EOF) {
+        if (c == ' ') {
+            blanks++;
+        } else {
+            /* flush blanks */
+            while (blanks > 0) {
+                int to_tab = TABSTOP - (col % TABSTOP);
 
+                if (blanks >= to_tab && to_tab > 1) {
+                    putchar('\t');
+                    col += to_tab;
+                    blanks -= to_tab;
+                } else {
+                    putchar(' ');
+                    col++;
+                    blanks--;
+                }
+            }
+
+            if (c == '\n') {
+                putchar('\n');
+                col = 0;
+            } else if (c == '\t') {
+                int to_tab = TABSTOP - (col % TABSTOP);
+                col += to_tab;
+                putchar('\t');
+            } else {
+                putchar(c);
+                col++;
+            }
+        }
+    }
+    return 0;
 }
-return 0;
-}
+
 /*hello####world
 whats up#########?*/
