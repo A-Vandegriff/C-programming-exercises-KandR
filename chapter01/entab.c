12b163e009e0030eb22c5fd32276a808c6fe7e46
A-Vandegriff <austinvandegriff@yahoo.com>
2026-01-12 19:20:36 -0500

    restarting

--- chapter01/entab.c
+++ chapter01/entab.c
@@ -9,42 +9,9 @@ int c;
 int col;
 int blanks;
 int tabs;
-tabs = 0;
-blanks = 0;
-col = 0;
 
 while((c = getchar()) != EOF){
-	if(c == '\n'){
-		printf("\n");
-		col = 0;
-	}else if(c == ' '){
-		while(c == ' '){
-			blanks++;
-			c = getchar();
-		}
-		if(blanks >= 8){
-			tabs = blanks/8;
-			col += tabs*8;
-			while(tabs > 0){
-				printf("\t");
-				tabs--;
-			}
-		}
-		while((blanks % TABSTOP) > 0){
-			printf(" ");
-			col++;
-			blanks--;
-		}
-		if(c != '\t'){
-			printf("%c", c);
-		}
-	}else if(c == '\t'){
-		
-	}else{
-		col++;
-		printf("%c", c);
-	}
-
+	
 }
 
 return 0;
