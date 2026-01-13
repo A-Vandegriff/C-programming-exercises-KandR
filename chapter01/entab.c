23c218b675da7702b10855acf135977e66e6ebfa
A-Vandegriff <austinvandegriff@yahoo.com>
2026-01-13 18:04:12 -0500

    restarting entab myself

--- chapter01/entab.c
+++ chapter01/entab.c
@@ -6,42 +6,7 @@ minimum number of tabs and blanks to acheive the same spacing*/
 
 int main(void)
 {
-    int c;
-    int col = 0;      /* current column */
-    int blanks = 0;   /* pending blanks */
 
-    while ((c = getchar()) != EOF) {
-        if (c == ' ') {
-            blanks++;
-        } else {
-            /* flush blanks */
-            while (blanks > 0) {
-                int to_tab = TABSTOP - (col % TABSTOP);
-
-                if (blanks >= to_tab && to_tab > 1) {
-                    putchar('\t');
-                    col += to_tab;
-                    blanks -= to_tab;
-                } else {
-                    putchar(' ');
-                    col++;
-                    blanks--;
-                }
-            }
-
-            if (c == '\n') {
-                putchar('\n');
-                col = 0;
-            } else if (c == '\t') {
-                int to_tab = TABSTOP - (col % TABSTOP);
-                col += to_tab;
-                putchar('\t');
-            } else {
-                putchar(c);
-                col++;
-            }
-        }
-    }
     return 0;
 }
 
