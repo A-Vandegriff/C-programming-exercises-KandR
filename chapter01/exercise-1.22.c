997a317e86677f483cba87e2b060383678c88f7f
A-Vandegriff <austinvandegriff@yahoo.com>
2026-01-14 15:13:02 -0500

    created comments on steps I need to solve to complete program

--- chapter01/exercise-1.22.c
+++ chapter01/exercise-1.22.c
@@ -4,6 +4,10 @@
   input. Make sure your program does something intelligent with very long
   lines, and if there are no blanks or tabs before the specified column.
 *******************************************************************************/
+//after last non-blank means a space between two words located right after the last non-blank
+//if the word (no blanks) passes through nth col, need to pick a fold spot (nth col)
+//for super long inputs the program should keep folding
+
 
 #include <stdio.h>
 
