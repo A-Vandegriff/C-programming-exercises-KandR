1d57806fd845c02c4de0a2b61da67fb03feef292
A-Vandegriff <austinvandegriff@yahoo.com>
2026-01-13 19:02:02 -0500

    almost done with entab

--- chapter01/entab.c
+++ chapter01/entab.c
@@ -6,7 +6,39 @@ minimum number of tabs and blanks to acheive the same spacing*/
 
 int main(void)
 {
+char c;
+int blanks;
+int col;
+int next_tab;
+//need to make an input stream
+while((c = getchar()) != EOF){
+	if(c == ' '){
+		blanks++;
+	}else{
+		//need to process potentially added blanks
+		next_tab = TABSTOP - (col % TABSTOP);
+		if(blanks >= next_tab && next_tab > 1){
+			putchar('\t');
+			col += next_tab;
+			blanks -= next_tab;
+		}else{
+			while(blanks > 0){
+				putchar(' ');
+				col++;
+				blanks--;
+			}
+		}
+	}
+	if(c == '\n'){
+		col = 0;
+		putchar('\n');
+	}else if(c == '\t'){
+		next_tab = TABSTOP - (col % TABSTOP);
+		col += next_tab;
+		putchar();
+	}
 
+}
     return 0;
 }
 
