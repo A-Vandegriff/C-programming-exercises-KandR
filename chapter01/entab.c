143547184ac4b1596a0bd558636ae5f1330f135c
A-Vandegriff <austinvandegriff@yahoo.com>
2026-01-15 13:49:19 -0500

    formatted entab, working on fold

--- chapter01/entab.c
+++ chapter01/entab.c
@@ -4,48 +4,46 @@ minimum number of tabs and blanks to acheive the same spacing*/
 
 #define TABSTOP 8
 
-int main(void)
-{
-int c;
-int blanks;
-int col;
-int next_tab;
-blanks = 0;
-col = 0;
+int main(void) {
+  int c;
+  int blanks;
+  int col;
+  int next_tab;
+  blanks = 0;
+  col = 0;
 
-//need to make an input stream
-while((c = getchar()) != EOF){
-	if(c == ' '){
-		blanks++;
-		continue;
-	}
-		while(blanks > 0){
-		//need to process potentially added blanks
-		next_tab = TABSTOP - (col % TABSTOP);
-		if(blanks >= next_tab && next_tab > 1){
-			putchar('\t');
-			col += next_tab;
-			blanks -= next_tab;
-		}else{
-				putchar(' ');
-				col++;
-				blanks--;
-		}
-	   }
-		if(c == '\n'){
-		col = 0;
-		putchar('\n');
-	}else if(c == '\t'){
-		next_tab = TABSTOP - (col % TABSTOP);
-		col += next_tab;
-		putchar('\t');
-	}else{
-		putchar(c);
-		col++;
-	}
-
-}
-    return 0;
+  // need to make an input stream
+  while ((c = getchar()) != EOF) {
+    if (c == ' ') {
+      blanks++;
+      continue;
+    }
+    while (blanks > 0) {
+      // need to process potentially added blanks
+      next_tab = TABSTOP - (col % TABSTOP);
+      if (blanks >= next_tab && next_tab > 1) {
+        putchar('\t');
+        col += next_tab;
+        blanks -= next_tab;
+      } else {
+        putchar(' ');
+        col++;
+        blanks--;
+      }
+    }
+    if (c == '\n') {
+      col = 0;
+      putchar('\n');
+    } else if (c == '\t') {
+      next_tab = TABSTOP - (col % TABSTOP);
+      col += next_tab;
+      putchar('\t');
+    } else {
+      putchar(c);
+      col++;
+    }
+  }
+  return 0;
 }
 
 /*hello####world
---
+++ chapter01/fold.c
@@ -0,0 +1,42 @@
+/*++****************************************************************************
+  Write a program to "fold" long input lines into two or more shorter lines
+  after the last non-blank character that occurs before the n-th column of
+  input. Make sure your program does something intelligent with very long
+  lines, and if there are no blanks or tabs before the specified column.
+*******************************************************************************/
+//after last non-blank means a space between two words located right after the last non-blank
+//if the word (no blanks) passes through nth col, need to pick a fold spot (nth col)
+//for super long inputs the program should keep folding
+
+#include <stdio.h>
+
+#define FOLD 15
+
+int main(void){
+
+int c;
+int col; //check leftover char of line arr, move them to front of new line
+int lastblank; //gets compared with prev blank, (make sure char is before blank)
+int line[FOLD];//array for line
+int i;
+col = 0;
+lastblank = 0;
+i = 0;
+
+//need to build line array WHILE calculating last blank
+while((c = getchar()) != EOF){
+	if(i < 14){
+	col++;
+	line[i] = c;
+		if(c == ' '){
+			if(){
+		
+			}
+		}
+	}
+
+}
+
+return 0;
+}
+
