3c67b508167db0c58228518b061081d1158c0e2f
A-Vandegriff <austinvandegriff@yahoo.com>
2026-01-19 16:03:10 -0500

    program folds, need to relook to understand more deeply

--- chapter01/fold.c
+++ chapter01/fold.c
@@ -4,38 +4,70 @@
   input. Make sure your program does something intelligent with very long
   lines, and if there are no blanks or tabs before the specified column.
 *******************************************************************************/
-//after last non-blank means a space between two words located right after the last non-blank
-//if the word (no blanks) passes through nth col, need to pick a fold spot (nth col)
-//for super long inputs the program should keep folding
+// after last non-blank means a space between two words located right after the
+// last non-blank if the word (no blanks) passes through nth col, need to pick a
+// fold spot (nth col) for super long inputs the program should keep folding
 
 #include <stdio.h>
 
-#define TABSTOP 8
-#define FOLD 15
+#define FOLD_COL 80
+#define MAXLINE 1000
 
-int main(void){
+int get_line(char line[], int max);
+void fold_line(char line[], int len);
 
-int c;
-int col; //check leftover char of line arr, move them to front of new line
-int lastblank; //gets compared with prev blank, (make sure char is before blank)
-char line[FOLD];//array for line
-int to_tab;
-int i;
-i = 0;
-col = 0;
-lastblank = 0;
-to_tab = 0;
+int main(void) {
+  char line[MAXLINE];
+  int len;
 
+  while ((len = get_line(line, MAXLINE)) > 0) {
+    fold_line(line, len);
+  }
 
-//need to build line array WHILE calculating last blank
-while((c = getchar()) != EOF){
-	//if i = 14 "fold"
-	//a blank at line[0] would not be after a char so dont count
-	//need to clear buffer when i > 14
-
-
-}
-return 0;
+  return 0;
 }
 
+/* read a line into s, return length */
+int get_line(char s[], int lim) {
+  int c, i;
 
+  for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++) {
+    s[i] = c;
+  }
+
+  if (c == '\n') {
+    s[i++] = c;
+  }
+
+  s[i] = '\0';
+  return i;
+}
+
+/* fold a line longer than FOLD_COL */
+void fold_line(char line[], int len) {
+  int i = 0;
+  int last_blank = -1;
+  int col = 0;
+
+  while (i < len) {
+    putchar(line[i]);
+
+    if (line[i] == ' ' || line[i] == '\t') {
+      last_blank = i;
+    }
+
+    col++;
+    i++;
+
+    if (col >= FOLD_COL) {
+      if (last_blank != -1) {
+        putchar('\n');
+        i = last_blank + 1;
+      } else {
+        putchar('\n');
+      }
+      col = 0;
+      last_blank = -1;
+    }
+  }
+}
